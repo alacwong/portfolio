1d799b20e90e4f725cad90f76d818589f972fc60
alacwong <mr6902spades@gmail.com>
2021-02-09 01:29:28 -0500

    fix: particle background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 import React from "react";
 import Home from "./pages/home/components/home";
+import Particles from "react-particles-js";
 
 function App() {
   return (
-      <Home></Home>
+      <div>
+          <h1>Hello</h1>
+          <Particles
+              params={{
+                  "particles": {
+                      "number": {
+                          "value": 200
+                      },
+                      "size": {
+                          "value": 3
+                      }
+                  },
+                  "interactivity": {
+                      "events": {
+                          "onhover": {
+                              "enable": true,
+                              "mode": "repulse"
+                          }
+                      }
+                  }
+              }} />
+      </div>
+
   );
 }
 
